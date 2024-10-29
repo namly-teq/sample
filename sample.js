8ee58fe427196e2b636945f69ea31abd4e21065e
namly-teq <67770153+namly-teq@users.noreply.github.com>
2024-10-29 09:21:01 +0700

    adding sample js for the modal

---
+++ sample.js
@@ -0,0 +1,29 @@
+(function () {
+  const btnCheck = document.getElementById("id-aux-close-modal");
+  console.log(btnCheck);
+  if (btnCheck) {
+    btnCheck.addEventListener("click", () => {
+      closeModal();
+    });
+  }
+  function closeModal() {
+    const modal2 = document.getElementById("id-aux-modal");
+    const modal = document.getElementById("id-aux-modal-overlay");
+    if (modal) {
+      modal.style.display = "none";
+    }
+    if (modal2) {
+      modal2.style.display = "none";
+    }
+  }
+  function logTreatmentInteractions() {
+    console.log("call treatment");
+    alert("Call Treatment");
+  }
+  const imgLog = document.getElementById("id-aux-modal-img");
+  if (imgLog) {
+    imgLog.addEventListener("click", () => {
+      logTreatmentInteractions();
+    });
+  }
+})();
